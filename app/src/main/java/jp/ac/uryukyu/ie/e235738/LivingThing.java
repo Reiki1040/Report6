db66f55b1b7c3dc9689fa4deb3856fee4dc7eb36
Echigo reiki <e235738@ie.u-ryukyu.ac.jp>
2023-12-21 14:09:39 +0900

    フィールド等簡易実装

--- app/src/main/java/jp/ac/uryukyu/ie/e235738/LivingThing.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e235738/LivingThing.java
@@ -1,5 +1,69 @@
 package jp.ac.uryukyu.ie.e235738;
 
+//育成キャラ、ボスキャラクラスのスーパークラス。
 public class LivingThing {
+    private String name;
+    private double HP;
+    private double power;
+    private double defence;
+    private double avoid;
+    private double hit;
     
+
+    public LivingThing(String name,double HP,double power,double defence,double avoid,double hit){
+        this.name = name;  //名前
+        this.HP = HP;  //ヒットポイント
+        this.power = power;  //筋力
+        this.defence = defence;  //丈夫さ
+        this.avoid = avoid;  //回避率
+        this.hit = hit;  //命中率
+    }
+    //以下、setter,getterメソッド
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setHP(double HP){
+        this.HP = HP;
+    }
+
+    public void setPower(double power){
+        this.power = power;
+    }
+
+    public void setDefence(double defence){
+        this.defence = defence;
+    }
+
+    public void setAvoid(double avoid){
+        this.avoid = avoid;
+    }
+    
+    public void setHit(double hit){ 
+        this.hit = hit;  
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public double getHP(){
+        return this.HP;
+    }
+
+    public double getPower(){
+        return this.power;
+    }
+
+    public double getDefence(){
+        return this.defence;
+    }
+
+    public double getAvoid(){
+        return this.avoid;
+    }
+
+    public double getHit(){
+        return this.hit;
+    }
 }
--- app/src/main/java/jp/ac/uryukyu/ie/e235738/NurtureChara.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e235738/NurtureChara.java
@@ -1,5 +1,24 @@
 package jp.ac.uryukyu.ie.e235738;
+import java.util.ArrayList;
 
-public class NurtureChara {
+public class NurtureChara extends LivingThing{
+    private double loyalty;
+    private double tired;
+    
+
+    NurtureChara(String name,double HP,double power,double defence,double avoid,double hit,double loyalty,double tired){
+        super(name,HP,power,defence,avoid,hit);
+        this.loyalty = loyalty;
+        this.tired = tired;
+    }
+
+    public void greetBadCondition(){
+        System.out.println(this.getName()+"  「イライラする...」"); //起こされた時の台詞
+    }
+
+    public void greetGoodCondition(){
+        System.out.println(this.getName()+"  「おはようございます!!」");  //起こされた時の台詞
+    }
+
     
 }
